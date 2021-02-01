a9b6a32bab2058e0c5806d260cc02ac9036e8921
syahjamal <sulaimansyahjamal@gmail.com>
2021-02-02 04:28:50 +0700

    add hooks & body component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { Button } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false)
+
+  function toggle() {
+    setIsActive(!isActive)
+  }
+
+  function reset() {
+    setSeconds(0);
+    setIsActive(false)
+  }
+
+  useEffect(()=>{
+    let interval = null;
+    if(isActive){
+      interval = setInterval(()=>{
+        setSeconds(seconds=>seconds + 1);
+      }, 1000);
+    } else if(!isActive && seconds !== 0){
+      clearInterval(interval);
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <div className="time">
+        {seconds}s
+      </div>
+      <div className="row">
+        <Button 
+        className={`button button-primary`} 
+        variant='contained'
+        color='primary'
+        onClick={toggle}
         >
-          Learn React
-        </a>
-      </header>
+          {isActive ? 'Pause' : 'Start'}
+        </Button>
+        <Button 
+          className="button" 
+          variant="outlined" 
+          color="secondary" 
+          onClick={reset}
+          > 
+          Reset
+        </Button>
+      </div>
     </div>
   );
 }
